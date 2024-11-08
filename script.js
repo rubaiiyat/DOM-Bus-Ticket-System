a8a587ff0d4452626dabda02db137557188c594b
rubaiiyat <abirr1516@gmail.com>
2024-11-08 23:47:23 +0600

    Work with Dom

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("working");
+const seatNumbers = document.querySelectorAll(".numbers");
+
+seatNumbers.forEach((number) => {
+  number.addEventListener("click", function () {
+    const getSeatNumber = number.innerText;
+
+    const addBackground = document.getElementById(getSeatNumber);
+    addBackground.classList.add("bg-green-500");
+    addBackground.classList.add("text-black");
+  });
+});
