df47db11409c0ad8e4d2afe3adc8d274c78038d0
rubaiiyat <abirr1516@gmail.com>
2024-11-09 23:13:04 +0600

    Add coupon code

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 const seatNumbers = document.querySelectorAll(".numbers");
+let seatCount = 0;
+
+const applyCouponBtn = document.getElementById("couponApplyBtn");
+applyCouponBtn.disabled = true;
 
 seatNumbers.forEach((number) => {
   number.addEventListener("click", function () {
-    const getSeatNumber = number.innerText;
+    seatCount += 1;
+    if (seatCount <= 4) {
+      const getSeatNumber = number.innerText;
 
-    const addBackground = document.getElementById(getSeatNumber);
-    addBackground.classList.add("bg-green-500");
-    addBackground.classList.add("text-black");
+      const addBackground = document.getElementById(getSeatNumber);
+      addBackground.classList.add(
+        "bg-green-500",
+        "text-black",
+        "hover:bg-green-600"
+      );
+
+      const tableList = document.getElementById("tableList");
+
+      const createTableRow = document.createElement("tr");
+      createTableRow.innerHTML = `
+
+      <td>${getSeatNumber}</td>
+      <td>Economy</td>
+      <td>550</td>
+
+    `;
+      tableList.appendChild(createTableRow);
+
+      // add subtotal
+      const subTotalPrice = document.getElementById("subTotalPrice");
+      subTotalPrice.innerText = `${seatCount * 550}`;
+
+      if (seatCount == 4) {
+        applyCouponBtn.disabled = false;
+      }
+    } else {
+      const seatError = document.getElementById("seatError");
+      seatError.innerText = "Sorry, you’ve reached the 4-seat limit.";
+
+      seatError.classList.add("text-red-500");
+    }
   });
 });
