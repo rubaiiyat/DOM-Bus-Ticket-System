1545d3d7fdd12ca81fb9e75a9ee70bdfdf28594e
rubaiiyat <abirr1516@gmail.com>
2024-11-09 23:44:39 +0600

    add coupon code option

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const seatNumbers = document.querySelectorAll(".numbers");
 let seatCount = 0;
 
 const applyCouponBtn = document.getElementById("couponApplyBtn");
-applyCouponBtn.disabled = true;
+applyCouponBtn.disabled = false;
 
 seatNumbers.forEach((number) => {
   number.addEventListener("click", function () {
@@ -31,11 +31,39 @@ seatNumbers.forEach((number) => {
 
       // add subtotal
       const subTotalPrice = document.getElementById("subTotalPrice");
-      subTotalPrice.innerText = `${seatCount * 550}`;
+      const inititalTotalPrice = seatCount * 550;
+      subTotalPrice.innerText = inititalTotalPrice;
 
-      if (seatCount == 4) {
-        applyCouponBtn.disabled = false;
-      }
+      // Grand Total
+      const grandTotalPrice = document.getElementById("grandTotal");
+      const grandTotal = inititalTotalPrice;
+      grandTotalPrice.innerText = grandTotal;
+
+      // if (seatCount == 4) {
+      applyCouponBtn.disabled = false;
+
+      applyCouponBtn.addEventListener("click", function () {
+        const couponError = document.getElementById("couponError");
+        const couponInput = document.getElementById("couponInput");
+        couponInputValue = couponInput.value;
+
+        if (couponInputValue == "NEW15") {
+          const discount = grandTotal * (15 / 100);
+          const afterdiscount = grandTotal - discount;
+          grandTotalPrice.innerText = afterdiscount;
+          couponError.innerText = "";
+        } else if (couponInputValue == "Couple 20") {
+          const discount = grandTotal * (20 / 100);
+          const afterdiscount = grandTotal - discount;
+          grandTotalPrice.innerText = afterdiscount;
+          couponError.innerText = "";
+        } else {
+          couponError.innerText = "The coupon code entered is not valid";
+
+          couponError.classList.add("text-red-500");
+        }
+      });
+      // }
     } else {
       const seatError = document.getElementById("seatError");
       seatError.innerText = "Sorry, you’ve reached the 4-seat limit.";
